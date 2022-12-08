7639a61c54727f99f915e19b92ca1f7c05a4878d
lowqwroby <eltiobvs@gmail.com>
2022-12-08 20:30:06 +0300

    Добавьте файлы проекта.

---
+++ Task_4_1_PM01_N2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Variant10
+{
+	class Program
+	{
+		static double rec(double x, int n, int index = 0)
+		{
+			if (index == n)
+			{
+				 return x + n;
+			}
+			else
+			{
+				return index + (x / rec(x, n, index + 1));
+			}
+		}
+		static void Main()
+		{
+			try
+			{
+				int n;
+				double x;
+				Console.Write("Введите x: "); x = Convert.ToDouble(Console.ReadLine());
+				Console.Write("Введите n: "); n = Convert.ToInt32(Console.ReadLine());
+				if(n <= 0)
+				{
+					throw new FormatException();
+				}
+				Console.WriteLine("Результат: {0:f3}", rec(x, n));
+			}
+			catch(FormatException)
+			{
+				Console.WriteLine("Введены неверные параметры.");
+			}
+			catch
+			{
+				Console.WriteLine("Что-то пошло не так...");
+			}
+		}
+	}
+}
---
+++ Task_4_2_PM01_N2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Variant10
+{
+	class Program
+	{
+		static int res(string str, int index, int i = 0)
+		{
+			int result = 0;
+			if (index < 0)
+			{
+				return result;
+			}
+			else
+			{
+				if (str[index] == '1')
+				{
+					result += Convert.ToInt32(Math.Pow(2, i));
+					return result + res(str, index - 1, i + 1);
+				}
+				else
+				{
+					return result + res(str, index - 1, i + 1);
+				}
+			}
+		}
+		static void Main()
+		{
+			Console.Write("Введите число в двоичной форме: ");
+			string str = Console.ReadLine();
+			string str1 = "";
+			int a = 0;
+			if (str[0] == '-')
+			{
+				a = -1;
+				for (int i = 1; i < str.Length; i++)
+				{
+					if (str[i] != '1' && str[i] != '0')
+					{
+						Console.WriteLine("Число не в двоичной форме!");
+						return;
+					}
+				}
+				for (int i = 1; i < str.Length; i++)
+				{
+					str1 += str[i];
+				}
+			}
+			else
+			{
+				a = 1;
+				for(int i = 0; i < str.Length; i++)
+				{
+					if (str[i] != '1' && str[i] != '0')
+					{
+						Console.WriteLine("Число не в двоичной форме!");
+						return;
+					}
+				}
+				for(int i = 0; i < str.Length; i++)
+				{
+					str1 += str[i];
+				}
+			}			
+			Console.WriteLine("Результат: {0}", (a * res(str1, str1.Length - 1)));
+		}
+	}
+}
